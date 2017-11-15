ac02c7f3be5d63ec778c0aa3d7984f95169eec83
Jorjon <jorjon@mantaray.xyz>
2017-11-15 20:10:58 -0300

    Big bang

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# friendpaste-api
-Simple Python API for friendpaste.com
+# Friendpaste API
+
+## Installation
+`pip install friendpaste-api`
+
+## Requirements
+- [requests](http://docs.python-requests.org/en/master/)
+
+## Usage
+```python
+from friendpaste import post_text
+
+# Update post using hash
+post_text('My new title', 'My new text'. 'y0Z1ia3h47j8ddRSn2QiPy')
+```
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'friendpaste-api',
+  packages = ['friendpaste-api'],
+  version = '0.1',
+  description = 'Simple Python API for friendpaste.com',
+  author = 'Jorjon',
+  author_email = 'jorjon@jorjon.com',
+  url = 'https://github.com/Jorjon/friendpaste-api',
+  download_url = 'https://github.com/Jorjon/friendpaste-api/archive/0.1.tar.gz',
+  keywords = ['friendpaste', 'api', 'scrapper'],
+  classifiers = [],
+)
